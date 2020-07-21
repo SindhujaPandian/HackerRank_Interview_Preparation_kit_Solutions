224dbf7a71279fd4fe427ee24014921d81238752
Sindhuja Pandian <51230035+SindhujaPandian@users.noreply.github.com>
2020-07-22 02:49:42 +0530

    Arrays

--- 2D_Arrays_DS.py
+++ 2D_Arrays_DS.py
@@ -1,10 +1,12 @@
-a=[]
+sum = []
+arr=[]
 for i in range(6):
-    a.append(list(map(int,input().split())))
-maxval=0
-for i in range(4):
-    for j in range(4):
-        total=a[i][j]+a[i][j+1]+a[i][j+2]+a[i+1][j+1]+a[i+2][j]+a[i+2][j+1]+a[i+2][j+2]
-        if(maxval<total):
-            maxval=total
-print(maxval)
+    arr.append(list(map(int,input().split())))
+
+
+
+for i in range(len(arr)-2):
+    for j in range(len(arr)-2):
+        sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+        
+print(max(sum))
