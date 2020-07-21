51f8d6011e153ac7369b87f0f46b27462ca72011
Sindhuja Pandian <51230035+SindhujaPandian@users.noreply.github.com>
2020-07-21 14:51:54 +0530

    Delete Left_Rotation.py

--- Left_Rotation.py
+++
@@ -1,7 +0,0 @@
-n=list(map(int,input().split()))
-size,num=n[0],n[1]
-arr=list(map(int,input().split()))
-for i in range(num):
-    arr=arr[1:]+arr[:1]
-for i in range(size):
-    print(arr[i],end=' ')
