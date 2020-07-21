03c1a4fa0a8a8e7e693f3dda7db8394421f51582
Sindhuja Pandian <51230035+SindhujaPandian@users.noreply.github.com>
2020-07-22 02:41:59 +0530

    warm-up challenge

--- Jumping_on_the_Clouds.py
+++ Jumping_on_the_Clouds.py
@@ -1,12 +1,10 @@
-n=int(input())
-arr=list(map(int,input().split()))
-i,count=0,-1
-while(i<n):
-    if(arr[i]!=1):
-        print(i,'->',end='')
-        count+=1
-    if(i!=n-1 and arr[i+2]!=0):
-        i+=1
-    else:
-        i+=2
-print('\ncount = ',count)
+def jumpingOnClouds(c):
+    if len(c) == 1 : return 0
+    if len(c) == 2: return 0 if c[1]==1 else 1
+    if c[2]==1: 
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2]==0:
+        return 1 + jumpingOnClouds(c[2:])
+n = int(input())
+c = list(map(int,input().split(' ')))
+print(jumpingOnClouds(c))
